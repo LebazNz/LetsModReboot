22b25f19b18a0b1390e99de2f84b285b166a966e
LebazNz <lebaz92@yahoo.com>
2014-06-21 11:31:52 -0400

    base mod file

---
+++ src/main/java/com/nzabel/letsmodreboot/letsmodreboot.java
@@ -0,0 +1,33 @@
+package com.nzabel.letsmodreboot;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import sun.security.jca.GetInstance;
+
+@Mod(modid = "letsmodreboot", name= "letsmodreboot", version = "1.7.2-1.0")
+public class letsmodreboot
+{
+    @Mod.Instance("letsmodreboot")
+    public static letsmodreboot instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void Init(FMLInitializationEvent e)
+    {
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e)
+    {
+
+    }
+
+}
